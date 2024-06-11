75778c39a02b76f75987752f1003695b198f92c7
GAM3RG33K <hj2931996@gmail.com>
2024-06-11 11:48:00 +0530

    Add authorization check for incoming requests on auth apis

--- final_project/index.js
+++ final_project/index.js
@@ -8,15 +8,26 @@ const app = express();
 
 app.use(express.json());
 
-app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
+app.use("/customer", session({ secret: "fingerprint_customer", resave: true, saveUninitialized: true }))
 
-app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+app.use("/customer/auth/*", function auth(req, res, next) {
+    const token = req.headers['Authorization'];
+    if (!token) {
+        return res.status(403).send('You are not authorized to access this resource.');
+    }
+
+    const payload = jwt.verify(token, "fingerprint_customer");
+    const user = payload.user;
+    if (!user) {
+        return res.status(403).send('Invalid user, You are not authorized to access this resource.');
+    }
+    req.user = user;
+    next();
 });
- 
-const PORT =5000;
+
+const PORT = 5000;
 
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
 
-app.listen(PORT,()=>console.log("Server is running"));
+app.listen(PORT, () => console.log("Server is running"));
